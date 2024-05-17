25a1dac3c57d5cc71d9b42b14b5c90e97c7cd2de
stevencara2009 <stevencara2009@gmail.com>
2024-05-16 20:43:56 -0500

    configuracion del servidor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hola mundo estudiantes");
+/*Import*/ 
+import express from 'express';
+
+/*Initializacion*/
+const app = express();
+
+/*Setting*/
+app.set('port', process.env.PORT || 3000);
+
+/*Middlewares*/
+
+
+/*routes
+Public files
+Run server*/
+app.listen(app.get('port'), () => {
+    console.log('server listening on port', app.get('port'));
+});
