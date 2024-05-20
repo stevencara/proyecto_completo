f18f588dea8b0defafd38281f98d9d955fc23b82
stevencara2009 <stevencara2009@gmail.com>
2024-05-20 17:52:04 -0500

    Config Plantillas, rutas y peticiones

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
-/*Import*/ 
+/* --------------------------------- Import --------------------------------- */
 import express from 'express';
+import morgan from 'morgan';
 
-/*Initializacion*/
+import {join, dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {engine} from 'express-handlebars';
+
+
+
+/* ----------------------------- Initializacion ----------------------------- */
 const app = express();
+/* ---------------- evitar colisiones de nombre de variables ---------------- */
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-/*Setting*/
+
+/* --------------------------------- Setting -------------------------------- */
 app.set('port', process.env.PORT || 3000);
+app.set('views', join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
-/*Middlewares*/
+
+/* ------------------------------- Middlewares ------------------------------ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 
-/*routes
-Public files
-Run server*/
+/* --------------------------------- routes --------------------------------- */
+app.get('/', (req, res) => {
+    res.json({"message": "hola mundo"})
+});
+
+
+/* ------------------------------ Public files ------------------------------ */
+app.use(express.static(join(__dirname, 'public')));
+
+
+/* ------------------------------- Run server ------------------------------- */
+
 app.listen(app.get('port'), () => {
     console.log('server listening on port', app.get('port'));
 });
