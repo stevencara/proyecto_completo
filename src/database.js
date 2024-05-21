f98cc6916601f4ee9c37f9b5cadb9f5060612d3f
stevencara2009 <stevencara2009@gmail.com>
2024-05-20 20:24:14 -0500

    se realizo configuracion de la base de datos

---
+++ src/database.js
@@ -0,0 +1,12 @@
+import {createPool} from "mysql2/promise";
+
+const pool = createPool({
+    host: 'localhost',
+    port: '3306',
+    user: 'prueba01',
+    password: 'prueba01',
+    database: 'prueba01'
+});
+
+/* --------- exportar la variable para que otro archivo pueda usarla -------- */
+export default pool; 
