83e6939a288e249449aae40d8232ec933e739757
stevencara2009 <stevencara2009@gmail.com>
2024-05-21 20:29:01 -0500

    se creo la vista y el metodo para listar

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import {join, dirname} from 'path';
 import {fileURLToPath} from 'url';
 import {engine} from 'express-handlebars';
+import personasRoutes from './routes/personas.routes.js';
 
 
 
@@ -38,7 +39,7 @@ app.get('/', (req, res) => {
     res.render('index')
 });
 
-
+app.use(personasRoutes);
 
 
 /* ------------------------------ Public files ------------------------------ */
